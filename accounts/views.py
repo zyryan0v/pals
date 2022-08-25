88cb4ed62f8f5f88a91a3b78ed63d7515f3cce32
Pavel Zyryanov <xyryanov@outlook.lv>
2022-08-25 16:37:43 +0300

    Add pretty login

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.contrib.auth import authenticate, login
+from .forms import LoginForm
 
-# Create your views here.
+def user_login(request):
+    if request.method == "POST":
+        form = LoginForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect("index")
+        else:
+            return render(request, "accounts/login.html", {
+                "form": form, 
+            })
+
+    else:
+        form = LoginForm
+        return render(request, "accounts/login.html", {
+            "form": form, 
+        })
+
--- pals/urls.py
+++ pals/urls.py
@@ -15,12 +15,13 @@ Including another URLconf
 """
 from django.conf import settings
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf.urls.static import static
 
 from landing import views as landv
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", landv.index),
+    path("", landv.index, name="index"),
+    path("accounts/", include("accounts.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
